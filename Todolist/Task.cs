f6bb762c1a44e4e1b4b73f7de931cd813637a2bc
Neto Rodrigues <jrodrigueson@gmail.com>
2022-06-24 09:24:54 -0300

    🎨 removes weather forecast references

---
+++ Todolist/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace Todolist
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
