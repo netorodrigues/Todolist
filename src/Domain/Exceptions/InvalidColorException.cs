84da8f771b851719b409d65d4911f45fc19cdf36
Neto Rodrigues <jrodrigueson@gmail.com>
2022-08-08 09:18:07 -0300

    ✨adds hexcolor handling

---
+++ src/Domain/Exceptions/InvalidColorException.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Exceptions
+{
+    public sealed class InvalidColorException : DomainException
+    {
+        public InvalidColorException(string entity, string colorValue) : base($"'{colorValue}' was an invalid value of color to {entity}") { }
+    }
+}
---
+++ src/Domain/ValueObjects/HexColor.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Exceptions;
+using System.Text.RegularExpressions;
+
+namespace Domain.ValueObjects
+{
+    public sealed class HexColor
+    {
+        public string Code { get; private set; }
+        private readonly string Pattern = "/^#[0-9A-F]{6}$/i";
+
+        public HexColor(string entity, string code)
+        {
+            if (!Regex.IsMatch(Pattern, code))
+            {
+                throw new InvalidColorException(entity, code);
+            }
+
+            Code = code;
+        }
+    }
+}
