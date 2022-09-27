34b41f9ae3fc3e88f0eabdaecc9a8468c06803e1
Neto Rodrigues <jrodrigueson@gmail.com>
2022-09-27 09:40:31 -0300

    adds initial create task command

---
+++ src/Application/Commands/CreateTaskCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Commands
+{
+    public class CreateTaskCommand
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? ScheduledDate { get; set; }
+        public CreateTaskCommand(string title, string description, DateTime? scheduledDate)
+        {
+            Title = title;
+            Description = description;
+            ScheduledDate = scheduledDate;
+        }
+    }
+}
