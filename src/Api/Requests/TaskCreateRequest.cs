0a379f71dabc52d1adb2ce031f6e048c79016a99
Neto Rodrigues <jrodrigueson@gmail.com>
2022-09-27 09:39:37 -0300

    adds a task create request

---
+++ src/Api/Requests/TaskCreateRequest.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Api.Requests
+{
+    public class TaskCreateRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? ScheduledDate { get; set; }
+    }
+
+    public class TaskCreateRequestValidator : AbstractValidator<TaskCreateRequest>
+    {
+        public TaskCreateRequestValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+        }
+    }
+}
