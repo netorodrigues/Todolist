db2684b739193d1072db87f5b309ddec36fa537b
Neto Rodrigues <jrodrigueson@gmail.com>
2022-12-17 15:26:13 -0300

    removes unused constructor

--- src/Application/Commands/CreateTaskCommand.cs
+++ src/Application/Commands/CreateTaskCommand.cs
@@ -7,11 +7,5 @@ namespace Application.Commands
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime? ScheduledDate { get; set; }
-        public CreateTaskCommand(string title, string description, DateTime? scheduledDate)
-        {
-            Title = title;
-            Description = description;
-            ScheduledDate = scheduledDate;
-        }
     }
 }
